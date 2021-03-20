0f63157140b92ae4a8fa424568885f0319e3df7d
Jessica Culver <jculver@us-l-1560.hsd1.co.comcast.net>
2021-03-20 07:51:38 -0600

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CompaniesList from './screens/CompaniesList/CompaniesList';
@@ -15,4 +16,30 @@ function App() {
     </div>
   )
 }
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+>>>>>>> 9f28bd4... Initialize project using Create React App
 export default App;
